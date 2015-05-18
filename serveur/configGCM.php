ecbf6531ed2ad0842c2df71a3df3dfd49107593a
diyfr <lgs.sgs2@gmail.com>
2015-05-18 12:23:06 +0200

    Create config.gcm

---
+++ serveur/configGCM.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database config variables
+ * A adapter en fonction de votre base de données.
+ */
+
+define("DB_HOST", "localhost");
+define("DB_USER", "gcm_dbuser");
+define("DB_PASSWORD", "{PASSWORD}");
+define("DB_DATABASE", "gcm_db");
+
+/*
+ * Google API Key à placer ici !!
+ */
+define("GOOGLE_API_KEY", "{GOOGLE_API}"); 
+?>
